e0e9e95825409a238a3c688f085838c304721791
edensongjbs <edensongjbs@gmail.com>
2020-10-08 17:45:45 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+
+def collect_multiples(lim)
+    col = []
+    (1...lim).each do |num|
+        if num%3===0 || num%5===0
+            col << num
+        end
+    end
+    col
+end
+
+def sum_multiples(lim)
+    collect_multiples(lim).reduce(0) { |sum, num| sum+num }
+end 
